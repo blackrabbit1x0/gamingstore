2cd68ea80b33fa0db8759e155fd9cea5aec67aa6
blackrabbit1x0 <128014667+blackrabbit1x0@users.noreply.github.com>
2025-05-27 23:30:22 +0545

    Configure Docker Setup

--- games/components/connect.php
+++ games/components/connect.php
@@ -2,7 +2,7 @@
 
 $db_name = 'mysql:host=db;dbname=suyash_game';
 $user_name = 'root';
-$user_password = 'root';
+$user_password = file_get_contents('/run/secrets/db_root_password');
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
